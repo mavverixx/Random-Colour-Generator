286c758af5de8fc11def8b172457a6861797cb44
Rikki Henry <rikkidhenry@gmail.com>
2025-01-04 18:41:36 +0000

    Commit Full version
    The Random Colour Generator is a fun and interactive tool that generates a random colour with a single click. It changes the background (or an element's colour) to a randomly selected RGB value and displays the colour code for reference. The design is simple, responsive, and easy to use, making it perfect for exploring colours or design inspiration.

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("JavaScript is connected!");
+
+// Get the button element
+const button = document.querySelector('#generate-btn');
+const colourBox = document.querySelector('#colour-box');
+
+
+button.addEventListener("click", function() {
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    colourBox.style.backgroundColor = randomColor;
+    console.log("the button to generate has been pressed")
+});
+
