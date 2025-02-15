39b68bf526ac056599bd03a73a0c67d01c8a53a8
Rikki Henry <rikkidhenry@gmail.com>
2025-02-15 19:20:01 +0000

    css hover button

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const colourBox = document.querySelector('#colour-box');
 
 
 button.addEventListener("click", function() {
-    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, 
+    ${Math.floor(Math.random() * 256)})`;
     colourBox.style.backgroundColor = randomColor;
     console.log("the button to generate has been pressed")
 });
